370b012636929150be10446072e189f92e2e988e
Buadcioq <Beibarysaldabergenov@gmail.com>
2022-10-15 15:09:21 +0600

    first commit

---
+++ js/test.js
@@ -0,0 +1,80 @@
+"use strict";
+
+a = 15;
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+
+// const obj = {
+//     a: 50,
+//     name: "jhon",
+//     age: 25,
+//     isMarried: false
+// };
+
+// obj.a = 10;
+
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []]
+
+// const result = confirm("Are you here?")
+
+// console.log(result)
+
+
+// const answer = +prompt("Вам есть 18?", "18")
+
+// console.log(typeof(answer))
+
+
+// const answer = [];
+
+
+
+// answer[0] = prompt('Как ваше имя', '');
+// answer[1] = prompt('Как ваше имя 2', '');
+// answer[2] = prompt('Как ваше имя 3', '');
+
+// console.log(typeof(answer));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = 'Beibarys';
+
+// alert(`hi, ${user}`)
+
+
+
+const arr = [1, 2, 3];
+const arrObj = {
+    0: 1,
+    1: 2,
+    2: 3
+}
+
+arrObj.b = '1234'
+
+console.log(arr[1]);
+console.log(arrObj['b'])
+
+const obj = {
+    'Anna' : 500,
+    'Alice' : 800
+};
+
+// console.log(name)
+// var name = "Ivan";
+
+// {
+//     let result = 50
+// }
+
+// console.log(result);
+
+
